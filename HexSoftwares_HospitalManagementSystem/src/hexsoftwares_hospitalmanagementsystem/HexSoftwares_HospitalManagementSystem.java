83766c4c70338bfafdbf622b24189d6db1e360ff
Macaza Tshabalala <macazatshabalala726@gmail.com>
2024-11-27 16:08:18 +0200

    Proper project

---
+++ HexSoftwares_HospitalManagementSystem/src/hexsoftwares_hospitalmanagementsystem/HexSoftwares_HospitalManagementSystem.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hexsoftwares_hospitalmanagementsystem;
+
+/**
+ *
+ * @author Tshabalala MG
+ */
+public class HexSoftwares_HospitalManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello");
+        // TODO code application logic here
+    }
+    
+}
